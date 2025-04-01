f0e4ff6efd58c7857ed376841c1b7c7eae1c420c
KAROLAYNE DA SILVA ALEXANDRE <karolayne.alexandre@univille.br>
2025-04-01 19:44:25 -0300

    feat: vetores

--- vetores/VetorApp.java
+++ vetores/VetorApp.java
@@ -1,24 +1,50 @@
 import estruturas.Lista;
-
+import estruturas.Vetor;
 
 public class VetorApp {
 
     public static void main(String[] args) {
-        Lista lista = new Lista();
-        
-        lista.adicionarAoInicio(28);
-        lista.adicionarAoInicio(27);
-        lista.adicionarAoInicio(24);
-        lista.adicionarAoInicio(21);
-        lista.adicionarAoInicio(32);
-        lista.adicionarAoInicio(12);
-        lista.adicionarAoInicio(2);
 
-        lista.mostrarNos();
+        // Criando um vetor
+        Vetor lista = new Vetor(10);
 
-        System.out.println("-----");
+        // Adicionando elementos ao vetor
+        int[] valores = { 2, 7, 9, 12, 16, 21, 27, 33, 42, 54 };
+        for (int valor : valores) {
+            lista.adicionar(valor);
+        }
 
-        lista.remover(21);
-        lista.mostrarNos();
+        // Exibindo os elementos da lista
+        System.out.println("Lista inicial:");
+        lista.mostrarElementos();
+
+        // Testando a busca binária
+        System.out.println("Buscando o valor 21:");
+        lista.buscaBinaria(21);
+
+        System.out.println("Buscando o valor 27:");
+        lista.buscaBinaria(27);
+
+        // Testando a inserção no início da lista
+        System.out.println("Inserindo o valor 1 no início da lista:");
+        lista.inserirValorNoInicio(1);
+        lista.mostrarElementos();
+
+        // Lista lista = new Lista();
+
+        // lista.adicionarAoInicio(28);
+        // lista.adicionarAoInicio(27);
+        // lista.adicionarAoInicio(24);
+        // lista.adicionarAoInicio(21);
+        // lista.adicionarAoInicio(32);
+        // lista.adicionarAoInicio(12);
+        // lista.adicionarAoInicio(2);
+
+        // lista.mostrarNos();
+
+        // System.out.println("-----");
+
+        // lista.remover(21);
+        // lista.mostrarNos();
     }
 }
--- vetores/estruturas/Vetor.java
+++ vetores/estruturas/Vetor.java
@@ -2,16 +2,19 @@ package estruturas;
 
 public class Vetor {
 
-    public Integer[] elementos; // 3fc -> 4fd
+    public Integer[] elementos;
     private int tamanho;
     private int capacidade;
 
+    // Definindo a capacidade do vetor através do construtor.
     public Vetor(int capacidade) {
         this.elementos = new Integer[capacidade];
         this.capacidade = capacidade;
         this.tamanho = 0;
     }
 
+    // Se for necessário adicionar um valor na lista é preciso verificar a
+    // capacidade.
     public void adicionar(Integer valor) {
         if (this.capacidade == this.tamanho) {
             this.aumentarCapacidade();
@@ -21,11 +24,12 @@ public class Vetor {
         this.tamanho++;
     }
 
+    // Se a capacidade não for suficiente é preciso aumentar o tamanho da lista
     private void aumentarCapacidade() {
-        int novaCapacidade = this.capacidade + (this.capacidade/2);
+        int novaCapacidade = this.capacidade + (this.capacidade / 2);
         Integer[] novosElementos = new Integer[novaCapacidade];
 
-        for (int i =0; i<this.tamanho; i++) {
+        for (int i = 0; i < this.tamanho; i++) {
             novosElementos[i] = this.elementos[i];
         }
 
@@ -33,17 +37,19 @@ public class Vetor {
         this.capacidade = novaCapacidade;
     }
 
+    // Método para mostrar os elementos da lista a
     public void mostrarElementos() {
         System.out.println("-----");
         System.out.println("Elementos no vetor");
-        for (int i=0; i < tamanho; i++) {
+        for (int i = 0; i < tamanho; i++) {
             System.out.println(this.elementos[i]);
         }
         System.out.println("-----");
     }
 
+    // Método para verificar se contem um item desejado
     public void contem(Integer valor) {
-        for (int i=0; i < tamanho; i++) {
+        for (int i = 0; i < tamanho; i++) {
             if (this.elementos[i] == valor) {
                 System.out.println("Valor encontrado");
                 return;
@@ -52,22 +58,24 @@ public class Vetor {
         System.out.println("Valor não encontrado");
     }
 
+    // Para remover é preciso informar a posição
     public void remover(int posicao) {
         if (posicao > this.tamanho) {
             System.out.println("Posição informada é inválida");
             return;
         }
 
-        for (int i = posicao; i < this.tamanho-1; i++) {
-            this.elementos[i] = this.elementos[i+1];
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
         }
 
-        this.elementos[this.tamanho-1] = null;
+        this.elementos[this.tamanho - 1] = null;
         this.tamanho--;
     }
 
+    // Método para remover o valor especifico da lista
     public void removerPorValor(Integer valor) {
-        for (int i=0; i<this.tamanho; i++) {
+        for (int i = 0; i < this.tamanho; i++) {
             if (this.elementos[i] == valor) {
                 this.remover(i);
                 System.out.println("Item removido");
@@ -76,5 +84,39 @@ public class Vetor {
         }
 
         System.out.println("Item não encontrado");
+    }
+
+    // Exercicio 1 - busca binária
+    public int buscaBinaria(int valor) {
+        int inicio = 0;
+        int fim = this.tamanho - 1;
+
+        while (inicio <= fim) {
+            int meio = (inicio + fim) / 2;
+
+            if (this.elementos[meio] == valor) {
+                return meio; // Encontrou
+            } else if (this.elementos[meio] < valor) {
+                inicio = meio + 1;
+            } else {
+                fim = meio - 1;
+            }
+        }
+
+        return -1; // Não encontrado
+    }
+
+    // Exercicio 1 - inserir valor no inicio
+    public void inserirValorNoInicio(Integer valor) {
+        if (this.capacidade == this.tamanho) {
+            this.aumentarCapacidade();
+        }
+
+        for (int i = this.tamanho; i > 0; i--) {
+            this.elementos[i] = this.elementos[i - 1];
+        }
+
+        this.elementos[0] = valor;
+        this.tamanho++;
     }
 }
