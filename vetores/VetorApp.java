2b6d4ea38b890f78d478f82391eb6980d362c505
Karolayne <karolaynedsa16@gmail.com>
2025-04-03 16:31:26 -0300

    feat: iteradores

--- vetores/VetorApp.java
+++ vetores/VetorApp.java
@@ -1,3 +1,4 @@
+import estruturas.Iterador;
 import estruturas.Lista;
 import estruturas.Vetor;
 
@@ -8,14 +9,20 @@ public class VetorApp {
         Vetor vetor = new Vetor(10);
         Lista listaduplamenteencadeada = new Lista();
 
-        // Adicionando elementos ao vetor
-        int[] valores = { 2, 7, 9, 12, 16, 21, 27, 33, 42, 54 };
-        for (int valor : valores) {
-            vetor.adicionar(valor);
-        }
+        // Adicionando elementos ao vetor 
+        vetor.adicionar(2);
+        vetor.adicionar(7);
+        vetor.adicionar(9);
+        vetor.adicionar(12);
+        vetor.adicionar(16);
+        vetor.adicionar(21);
+        vetor.adicionar(27);
+        vetor.adicionar(33);
+        vetor.adicionar(42);
+        vetor.adicionar(54);
 
         // Exibindo os elementos da lista
-        System.out.println("Lista inicial:");
+        System.out.println("Vetor inicial:");
         vetor.mostrarElementos();
 
         // Testando a busca binária
@@ -29,29 +36,77 @@ public class VetorApp {
 
         // Testando a inserção no início da lista
         System.out.println("-----");
-        System.out.println("Inserindo o valor 1 no início da lista:");
+        System.out.println("Inserindo o valor no início da lista:");
         vetor.inserirValorNoInicio(1);
-        vetor.mostrarElementos();
+        vetor.mostrarElementos(); //mostrar a lista com o novo valor no inicio
 
+        //adicionando itens a lista
         listaduplamenteencadeada.adicionarAoInicio(9);
-        listaduplamenteencadeada.adicionarAoFim(10);
-        listaduplamenteencadeada.adicionarAoFim(20);
-        listaduplamenteencadeada.adicionarAoFim(30);
+        listaduplamenteencadeada.adicionarAoInicio(10);
+        listaduplamenteencadeada.adicionarAoInicio(20);
+        listaduplamenteencadeada.adicionarAoInicio(30);
+        
+       
+        System.out.println("Nós da lista:");
+        System.out.println("------");
+        listaduplamenteencadeada.mostrarNos();
 
         // Exibindo o número de nós na lista
-        System.out.println("Número de nós na lista: " + listaduplamenteencadeada.getTamanho());
+        System.out.println("------");
+        System.out.println("Número de nós na lista: " + listaduplamenteencadeada.contarNos());
 
         // Inserindo após um valor informado
         listaduplamenteencadeada.inserirApos(20, 25); // Inserindo 25 após 20
         listaduplamenteencadeada.inserirApos(10, 15); // Inserindo 15 após 10
-
-        // Exibindo a lista 
         listaduplamenteencadeada.exibirLista();
 
         // Testando a remoção
         listaduplamenteencadeada.remover(20);
         listaduplamenteencadeada.exibirLista();
+        
+        //Inserindo ao final da lista
+        System.out.println("Inserindo no fim da lista:");
+        listaduplamenteencadeada.adicionarAoFim(10);
+        listaduplamenteencadeada.adicionarAoFim(20);
+        listaduplamenteencadeada.adicionarAoFim(30);
+        listaduplamenteencadeada.mostrarNos();
 
+        System.out.println("------");
+        System.out.println("Número de nós na lista: " + listaduplamenteencadeada.contarNos());
+
+        //Inserindo ap´s um valor especifico
+        System.out.println("Inserindo 55 após o 10...");
+        listaduplamenteencadeada.inserirApos(10, 55);
+        listaduplamenteencadeada.mostrarNos();
+        System.out.println("------");
+
+        System.out.println("Insere 60 após o atual:");
+        Iterador it = listaduplamenteencadeada.getIterador(); //inicializando
+        it.proximo(); // anda para o segundo nó (índice 1)
+        it.inserirAposAtual(60);
+        listaduplamenteencadeada.mostrarNos();
+        System.out.println("------");
+
+        System.out.println("Remove o nó após o atual:");
+        it = listaduplamenteencadeada.getIterador();
+        it.proximo(); // volta ao segundo nó
+        it.removerAposAtual(); // remove o 99
+        listaduplamenteencadeada.mostrarNos();
+        System.out.println("------");
+
+        System.out.println("Inserindo 55 antes do nó atual:");
+        it = listaduplamenteencadeada.getIterador();
+        it.proximo(); // vai pro segundo nó
+        it.inserirAntesAtual(55);
+        listaduplamenteencadeada.mostrarNos();
+        System.out.println("------");
+
+        System.out.println("Removendo o nó antes do atual:");
+        it = listaduplamenteencadeada.getIterador();
+        it.proximo(); // vai pro segundo nó
+        it.removerAntesAtual();
+        listaduplamenteencadeada.mostrarNos();
+        System.out.println("------");
 
     }
 }
--- vetores/estruturas/Iterador.java
+++ vetores/estruturas/Iterador.java
@@ -55,4 +55,73 @@ public class Iterador {
         System.out.println("");
         this.proximo();
     }
+    public void inserirAposAtual(int valor) {
+        No novo = new No(valor);
+        No atual = this.atual;
+        //Cria um novo nó com o valor recebido e guarda a referência do nó atual - lembrando que permanece no escopo local
+        novo.anterior = atual;
+        novo.proximo = atual.proximo;
+        //O novo nó aponta para o nó atual como seu anterior e para o próximo nó como seu próximo
+        if (atual.proximo != null) {
+            atual.proximo.anterior = novo;
+        }
+        //existe um nó após o atual, esse nó passa a apontar para o novo nó como seu anterior
+        atual.proximo = novo;
+    }
+
+    public void removerAposAtual() {
+        if (this.atual.proximo == null) {
+            System.out.println("Não há nó após o atual.");
+            return;
+        }
+    
+        No remover = this.atual.proximo;
+        this.atual.proximo = remover.proximo;
+    
+        if (remover.proximo != null) {
+            remover.proximo.anterior = this.atual;
+        }
+    
+        remover.proximo = null;
+        remover.anterior = null;
+    }
+
+    public void inserirAntesAtual(int valor) {
+        No novo = new No(valor);
+        
+        if (this.atual.anterior == null) {
+            // insere no início da lista
+            // Verifica se o nó atual é o primeiro da lista.
+            novo.proximo = this.atual;
+            this.atual.anterior = novo;
+            this.lista.inicio = novo;
+        } else {
+            novo.anterior = this.atual.anterior;
+            novo.proximo = this.atual;
+            this.atual.anterior.proximo = novo;
+            this.atual.anterior = novo;
+        }
+    }
+
+    public void removerAntesAtual() {
+        if (this.atual.anterior == null) {
+            System.out.println("Não há nó antes do atual.");
+            return;
+        }
+    
+        No remover = this.atual.anterior;
+    
+        if (remover.anterior != null) {
+            remover.anterior.proximo = this.atual;
+            this.atual.anterior = remover.anterior;
+        } else {
+            // Era o primeiro nó
+            this.atual.anterior = null;
+            this.lista.inicio = this.atual;
+        }
+    
+        remover.proximo = null;
+        remover.anterior = null;
+    }
+    
 }
